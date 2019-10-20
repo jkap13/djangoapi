987c69ac3935a04c03eb501670c4230c1412e75b
Jai Kapoor <dec13jk@gmail.com>
2019-10-20 17:59:18 +0530

    added comment to serializer code

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Course
-
+#This is our serializer page
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Course
